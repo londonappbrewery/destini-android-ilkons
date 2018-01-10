8792b8bc0325c8b45bd4676094b34e4467a3afe0
ilkons <ilkons@gmail.com>
2018-01-10 22:53:26 +0200

    Completed

--- app/src/main/java/com/londonappbrewery/destini/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/destini/MainActivity.java
@@ -2,29 +2,106 @@ package com.londonappbrewery.destini;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     // TODO: Steps 4 & 8 - Declare member variables here:
-
+    TextView mStoryTextView;
+    Button  mButtonTop;
+    Button  mButtonBottom;
+    int mStoryIndex = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (savedInstanceState != null) {
+            mStoryIndex = savedInstanceState.getInt("StoryKey");
+        }
+
 
         // TODO: Step 5 - Wire up the 3 views from the layout to the member variables:
-
+        mStoryTextView = (TextView) findViewById(R.id.storyTextView);
+        mButtonTop = (Button) findViewById(R.id.buttonTop);
+        mButtonBottom = (Button) findViewById(R.id.buttonBottom);
+        updateLayout(mStoryIndex);
 
 
         // TODO: Steps 6, 7, & 9 - Set a listener on the top button:
 
+        mButtonTop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mStoryIndex == 3)  {
+                    mStoryIndex = 6;
+                } else  {               //In case we are in Story 1 or 2, no more possible values while the buttons are visible
+                    mStoryIndex = 3;
+                }
+             updateLayout(mStoryIndex);
 
+            }
+        });
 
 
         // TODO: Steps 6, 7, & 9 - Set a listener on the bottom button:
 
+        mButtonBottom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mStoryIndex == 1) {
+                    mStoryIndex = 2;
+                }
+                else if (mStoryIndex == 2) {
+                    mStoryIndex = 4;
+                }
+                else {
+                    mStoryIndex = 5;
+                }
+
+            updateLayout(mStoryIndex);
+            }
+        });
 
     }
+
+    // Update text in mStoryTextView and on buttons depending on step
+    private void updateLayout(int mStoryIndex) {
+        if (mStoryIndex == 1) {
+            mStoryTextView.setText(R.string.T1_Story);
+            mButtonTop.setText(R.string.T1_Ans1);
+            mButtonBottom.setText(R.string.T1_Ans2);
+        } else if (mStoryIndex == 2) {
+            mStoryTextView.setText(R.string.T2_Story);
+            mButtonTop.setText(R.string.T2_Ans1);
+            mButtonBottom.setText(R.string.T2_Ans2);
+        } else if (mStoryIndex == 3) {
+            mStoryTextView.setText(R.string.T3_Story);
+            mButtonTop.setText(R.string.T3_Ans1);
+            mButtonBottom.setText(R.string.T3_Ans2);
+        // when the mStoryIndex > 3 both buttons are invisible
+        } else if (mStoryIndex == 4) {
+            mStoryTextView.setText(R.string.T4_End);
+            mButtonTop.setVisibility(View.GONE);
+            mButtonBottom.setVisibility(View.GONE);
+        } else if (mStoryIndex == 5) {
+            mStoryTextView.setText(R.string.T5_End);
+            mButtonTop.setVisibility(View.GONE);
+            mButtonBottom.setVisibility(View.GONE);
+        } else if (mStoryIndex == 6) {
+            mStoryTextView.setText(R.string.T6_End);
+            mButtonTop.setVisibility(View.GONE);
+            mButtonBottom.setVisibility(View.GONE);
+        }
+
+    }
+
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("StoryKey", mStoryIndex);
+    }
+
 }
